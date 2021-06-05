5490f3a1148573746e3e5b5253f5f8760ca620f9
tubasa ito <tubasa.ito.1225@gmail.com>
2021-06-05 01:36:35 +0000

    add status

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_205542) do
+ActiveRecord::Schema.define(version: 2021_06_05_004658) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
