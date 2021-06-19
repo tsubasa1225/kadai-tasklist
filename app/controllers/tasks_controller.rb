96a5937509b7939f76931d0d7030f8aabf813a4f
tubasa ito <tubasa.ito.1225@gmail.com>
2021-06-19 08:08:32 +0000

    re:re:work

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,9 +5,7 @@ class TasksController < ApplicationController
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-    if logged_in?
-      @tasks = current_user.tasks.order(id: :desc)
-    end
+    @tasks = current_user.tasks.order(id: :desc)
   end
   
   def show
@@ -58,7 +56,8 @@ class TasksController < ApplicationController
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
-      redirect_to login_url
+      flash[:no_task] = "タスクがありません"
+      redirect_to root_url
     end
   end
 end
